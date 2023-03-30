88890ab538b24eabef80a5710274a165484b9b16
atulverma4796 <atulverma4796@gmail.com>
2023-03-30 17:47:22 +0530

    added cases for schedule text

--- src/extracting_texts.py
+++ src/extracting_texts.py
@@ -1,7 +1,7 @@
 from PyPDF2 import PdfReader
 import spacy
 
-reader = PdfReader("src/samples/scheduled_texts.pdf")
+reader = PdfReader("src/samples/schedule_text2.pdf")
 number_of_pages = len(reader.pages)
 nlp = spacy.load("en_core_web_sm")
 
